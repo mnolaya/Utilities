6c819783f8223e111a53bae951ec1bce4d7567f6
Michael Olaya <michael.n.olaya@gmail.com>
2020-01-17 09:06:02 -0500

    Updates to existing vid2img.m file to save timestamps for each video frame taken relative to original .mp4 file. Also added independent timestamp checker for each file without the need for creating images.

--- imgtstamp.m
+++ imgtstamp.m
@@ -1,11 +1,10 @@
 %% INPUTS
 
-prefix = '75OS';
 nFrames = 20;
 refTime = 4;
 startTime = 6;
-workingDir = pwd;
-minTime = 10;
+vidLoc = uigetdir('C:\');
+addpath(vidLoc)
 
 %% CREATE IMAGES
 % Creates new subdirectory for each specimen where images are placed if one
@@ -19,51 +18,21 @@ minTime = 10;
 % second to last frame
 % All videos are stored in the created or already existing subdirectory
 
-vidNames = dir('*.mp4');
+vidNames = dir(fullfile(vidLoc,'*.mp4'));
 nVids = length(vidNames);
 tStamps = zeros(nFrames+1,nVids);
 
 for i=1:nVids
 
-    specimen = char(regexp(vidNames(i).name,'S\d\d','match'));
-    subDir = [specimen,'-Images'];
-    switch exist(subDir,'dir')
-        case 0
-            mkdir(workingDir,subDir)
-        otherwise
-            continue
-    end
-    
-    vidObj = VideoReader(vidNames(i).name);
-    
-    switch true
-        case vidObj.Duration<=minTime
-            continue
-        otherwise
-    end
-    
+    specimen = char(regexp(vidNames(i).name,'S\d\d','match'));    
+    vidObj = VideoReader(vidNames(i).name);   
     vidObj.CurrentTime = refTime;    
-    img = readFrame(vidObj);
-    fileName = [prefix,'-',specimen,'_REF.jpg'];
-    fullName = fullfile(workingDir,subDir,fileName);
-    imwrite(img,fullName)
-
     startFrame = ceil(startTime*vidObj.FrameRate);
     frames = ceil(linspace(startFrame,vidObj.NumFrame-1,nFrames));
-    
-    tStamps(1,i) = vidObj.CurrentTime;
-    ii = 1;
-    
+        
     for j=1:nFrames
         currentTime = frames(j)/vidObj.FrameRate;
-        vidObj.CurrentTime = currentTime;
-        img = readFrame(vidObj);
-        fileName = [prefix,'-',specimen,'_',sprintf('%02d',ii),...
-            '.jpg'];
-        fullName = fullfile(workingDir,subDir,fileName);
-        imwrite(img,fullName)
-        
+        vidObj.CurrentTime = currentTime;     
         tStamps(j+1,i) = vidObj.CurrentTime;
-        ii = ii+1;
     end
 end
